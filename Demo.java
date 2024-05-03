b1a36821afb618fa3d4e58b425f92d64a6f829e3
renrui <15048361688@163.com>
2024-05-03 16:46:22 +0800

    dev分支添加内容

--- Demo.java
+++ Demo.java
@@ -1,16 +1,5 @@
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        //第一次修改，commit and push
-
-        //第三次修改，commit and push
-
         //dev分支添加内容
-
-        //dev在第10行添加内容
-
-        //dev分支添加新内容1
-
-        //dev分支添加新内容2
     }
 }
