1c27c26e65af0d8415386fe0b4668bb58b1a3280
renrui <15048361688@163.com>
2024-05-03 16:36:52 +0800

    main分支添加内容

--- Demo.java
+++ Demo.java
@@ -1,14 +1,5 @@
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        //第一次修改，commit and push
-
-        //dev分支添加内容
-
-        //main分支在第10行添加内容
-
-        //dev在第10行添加内容
-
         //main分支添加内容
     }
 }
